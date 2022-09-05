8b46d40a28f0d2a83ea15a1793414821fcb333ea
Jan Bianca O. Tejome <111472867+biancatejome@users.noreply.github.com>
2022-09-05 09:58:13 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+
+        StudentClass Me = new StudentClass();
+
+        Me.surname = "Tejome"; 
+        Me.firstName = "Jan Bianca";
+        Me.middleInitial = 'O';
+        Me.dateOfBirth = "01/02/2004";
+        Me.studentNumber = "2022-102430";
+        Me.studentEmailAddress = "tejomejo@students.national-u.edu.ph";
+        Me.iAmAwesome = true;
+    
+        Me.sayIntroduction();
+        Me.sayMyStudentNumber();
+        Me.sayMyEmailAddress();
+        Me.amIAwesome();
+    } 
+} 
