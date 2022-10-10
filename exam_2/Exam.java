40f05a53e3e29ddebb066c492315997bfa9bff7c
Jan Bianca O. Tejome <111472867+biancatejome@users.noreply.github.com>
2022-10-10 18:33:16 +0800

    Update Exam.java

--- exam_2/Exam.java
+++ exam_2/Exam.java
@@ -7,6 +7,15 @@ public class Exam {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
+        
+        // Long exam 2
+        // Total points possible (100 pts)
+        // Methods and Conditions
+
+        // Part I
+        // 1. Create a method.
+        // 2. Call the method.
+        // 3. Print result of the method.
 
         studentID();
         oddEven();
@@ -19,7 +28,10 @@ public class Exam {
         Selection();
         emailAd();
     }
-
+        // ==============================================================================
+        // 1. Create a method and think of a formula to get the last 6 digits of your
+        // student number. (10 pts)
+    
         static void studentID(){
         System.out.println("1. Last six digits");
         System.out.print("Enter your ten digit student number: ");
@@ -31,8 +43,11 @@ public class Exam {
 
         }
 
-        // // ==============================================================================
-
+        // ==============================================================================
+        // // 2. Create a method that will:
+        // // print "ODD" if the last two digits of your student number is an odd number
+        // // print "EVEN" if the last two digits of your student number is an even number.
+        // // (10 pts)
 
         static void oddEven(){
         System.out.println("2. ODD or EVEN numbers");
@@ -46,7 +61,9 @@ public class Exam {
         }
     
         // ==============================================================================
-
+        // 3. Create a method that will return the total character length of your
+        // firstname and your surname. (10 pts)
+    
         static void characLength(){
         System.out.println("3. Length of your fullname");
         System.out.print("Enter your firstname: ");
@@ -62,8 +79,11 @@ public class Exam {
 
         }
 
-//         // ==============================================================================
-
+        // ==============================================================================
+        // 4. Create a method to that will return true if your firstname in BOTH
+        // UPPERCASE and lowercase are EQUAL.
+        // Otherwise, return false.
+    
         static void upperLower(){
         System.out.println("4. Check your first name in LOWERCASE and UPPERCASE");
         System.out.print("Enter your first name in lowercase: ");
@@ -80,10 +100,11 @@ public class Exam {
         }
     }
         
-        // }
-
-//         // ==============================================================================
-
+         // ==============================================================================
+         // 5. Create a method that will return true if your name starts with
+         // a vowel 'A','E','I','O','U'.
+         // Otherwise, return false.
+      
         static void vowels(){
         System.out.println("5. Vowel or Consonant");
         System.out.print("Enter the first character of your first name: ");
@@ -111,8 +132,15 @@ public class Exam {
         }
     }
 
-//         // =========================================================================================================================
+        // =========================================================================================================================
+        // Part II
+         // Print "valid" if the result is true.
+         // Print "invalid" if the result is false.
 
+         // ==============================================================================
+         // 6. Create a condition to check if the entered student number is your student
+         // number. (10 pts)
+    
         static void validation(){
         System.out.println("6. Student number validation");
         System.out.print("Enter your ten digit student number: ");
@@ -125,9 +153,10 @@ public class Exam {
         }
 
     }
-
-//         // ==============================================================================
-
+         // ==============================================================================
+        // 7. Create a condition to check if your firstname is in UPPERCASE and your
+        // lastname is in lowercase. (10 pts)
+    
         static void sensitivity(){
         System.out.println("7. Case sensitivity check");
         System.out.print("Enter your firstname in UPPERCASE: ");
@@ -142,8 +171,17 @@ public class Exam {
             System.out.println("INVALID");
         }
     }
-//         // ==============================================================================
+         // ==============================================================================
+         // 8. Create a nested condition that will ask the specialization of the student
+         // is taking
+         // depending if the student is a BSIT or BSCS student.
 
+        // Condition 1
+        // if the student is BSIT, ask again if the student is taking MWAA or MAA.
+
+        // Condition 2
+        // if the student is BSCS, ask again if the student is taking ML or DF.
+    
         static void nested(){
         System.out.println( "8. Nested conditions");
         System.out.print( "Enter your enrolled course (BSIT or BSCS): ");
@@ -177,7 +215,17 @@ public class Exam {
         }
     }
 
-//         // ==============================================================================
+         // ==============================================================================
+         // 9. Create a condition to check if the answer belongs to one of the possible
+         // answers. (10 pts)
+
+         // STEM
+         // ICT
+         // ABM
+         // HUMSS
+         // HOME ECONOMICS
+         // ARTS AND DESIGN
+         // TVL MARITIME
 
         static void Selection(){
         System.out.println("9. Selection");
@@ -213,7 +261,8 @@ public class Exam {
             }
         }
         // ==============================================================================
-        
+        // 10. Create a condition to check if your student email address contains your
+        // surname. (10 pts)
         static void emailAd(){
         System.out.println ("10. Validate student email address");
         System.out.print("Enter your student email address: ");
@@ -226,5 +275,29 @@ public class Exam {
             }
         }
 
+ //         // ==============================================================================
+//         // Programming competition/Job interview level question (20 points)
+
+//         // 11. Think like an ATM.
+
+//         // You have an N amount of pesos in your savings account and you have decided
+//         // that you want to withdraw all of your money in the ATM.
+
+//         // The denomination of peso bills that the ATM could dispense are 20, 50, 100,
+//         // 200, 500, 1000.
+
+//         // Create a method that will return the minimum number of peso bills that the
+//         // ATM will dispense after withdrawing all of your money.
+
+//         // Example:
+//         // Elizer has 150 pesos in his bank account. If elizer withdraws 150 pesos in
+//         // the ATM, the ATM will dispense two peso bills (100 peso bill and 50 peso
+//         // bill). (Answer is 2)
+
+//         System.out.println("11. Think like an ATM");
+//         System.out.print("Enter the total amount of money in your bank account: ");
+//         int amount = scan.nextInt();
+
+//         // Close scanner
             scan.close(); 
     }
